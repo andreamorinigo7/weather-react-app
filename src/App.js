a75be34a6578837fcd2179817854206be471b205
andreamorinigo7 <morinigoandrea7@gmail.com>
2024-11-15 13:11:54 -0500

    added github repositiory link

--- src/App.js
+++ src/App.js
@@ -9,7 +9,10 @@ function App() {
         <footer>
           This project was coded by{" "}
           <a href="https://github.com/andreamorinigo7"> Andrea Morinigo</a> on{" "}
-          <a href="#"> Github</a>
+          <a href="https://github.com/andreamorinigo7/weather-react-app">
+            {" "}
+            Github
+          </a>
         </footer>
       </div>
     </div>
