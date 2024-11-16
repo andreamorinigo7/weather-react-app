4750c68083e76e022cac65fd49039505cadeb0fe
andreamorinigo7 <morinigoandrea7@gmail.com>
2024-11-16 08:21:16 -0500

    updated the css

--- src/App.js
+++ src/App.js
@@ -4,17 +4,15 @@ import Weather from "./Weather";
 function App() {
   return (
     <div className="App">
-      <div className="container">
-        <Weather />
-        <footer>
-          This project was coded by{" "}
-          <a href="https://github.com/andreamorinigo7"> Andrea Morinigo</a> on{" "}
-          <a href="https://github.com/andreamorinigo7/weather-react-app">
-            {" "}
-            Github
-          </a>
-        </footer>
-      </div>
+      <Weather />
+      <footer>
+        This project was coded by{" "}
+        <a href="https://github.com/andreamorinigo7"> Andrea Morinigo</a> on{" "}
+        <a href="https://github.com/andreamorinigo7/weather-react-app">
+          {" "}
+          Github
+        </a>
+      </footer>
     </div>
   );
 }
--- src/Weather.js
+++ src/Weather.js
@@ -12,7 +12,7 @@ export default function Weather() {
             placeholder="Search for cities ..."
             className="search-input col-9"
           />
-          <input className="col-3" type="submit" Search />
+          <input className="col-3 submit-input" type="submit" Search />
         </div>
       </form>
       <div className="row">
@@ -29,6 +29,24 @@ export default function Weather() {
           <h2>
             70 <span>°C | °F</span>
           </h2>
+        </div>
+        <div className="weather-description">
+          <p>CONDITIONS</p>
+          <div className="row">
+            <div className="col-6">
+              <p>Feels like:</p>
+              <h3>72</h3>
+              <p>Humidity</p>
+              <h3>80%</h3>
+            </div>
+            <div className="col-6">
+              <p>Wind Speed</p>
+              <h3>0.2 km/h</h3>
+
+              <p>UV index</p>
+              <h3>2</h3>
+            </div>
+          </div>
         </div>
       </div>
     </div>
