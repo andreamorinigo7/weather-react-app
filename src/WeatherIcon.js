158e033e92296634399a9cd29c36b76585f18176
andreamorinigo7 <morinigoandrea7@gmail.com>
2024-11-17 10:55:54 -0500

    weather icon mapped

--- src/WeatherIcon.js
+++ src/WeatherIcon.js
@@ -1,10 +1,30 @@
 import React from "react";
+import { WeatherSvg } from "weather-icons-animated";
 
 export default function WeatherIcon(props) {
+  console.log(props.code);
+  const codeMapping = {
+    "01d": "sunny",
+    "01n": "clear-night",
+    "02d": "partlycloudy",
+    "02n": "partlycloudy",
+    "03d": "cloudy",
+    "03n": "cloudy",
+    "04d": "partlycloudy",
+    "04n": "partlycloudy",
+    "09d": "pouring",
+    "09n": "pouring",
+    "10d": "rainy",
+    "10n": "rainy",
+    "11d": "lightning",
+    "11n": "lightning-rainy",
+    "13d": "snowy",
+    "13n": "snowy",
+    "50d": "fog",
+    "50n": "fog",
+  };
+
   return (
-    <img
-      src="https://github.com/shannonhochkins/weather-icons-animated/blob/main/output.gif?raw=true"
-      alt={props.alt}
-    />
+    <WeatherSvg state={codeMapping[props.code]} width={135} height={135} />
   );
 }
