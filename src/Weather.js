b91b15c631464b1f5e09e9abd563ee9fcdc6c370
andreamorinigo7 <morinigoandrea7@gmail.com>
2024-11-16 19:39:46 -0500

    search engine working and fixed axios bug

--- src/Weather.js
+++ src/Weather.js
@@ -1,14 +1,13 @@
 import React, { useState } from "react";
 import "./Weather.css";
 import axios from "axios";
-import FormattedDate from "./FormattedDate";
+import WeatherInfo from "./WeatherInfo";
 
 export default function Weather(props) {
   const [weatherData, setWeatherData] = useState({ ready: false });
   const [city, setCity] = useState(props.defaultCity);
 
   function handleResponse(response) {
-    console.log(response.data.time);
     setWeatherData({
       ready: true,
       temperature: response.data.temperature.current,
@@ -23,64 +22,42 @@ export default function Weather(props) {
     });
   }
 
+  function handleSubmit(event) {
+    event.preventDefault();
+    search();
+  }
+
+  function handleCityChange(event) {
+    setCity(event.target.value);
+  }
+
+  function search() {
+    const apiKey = "o7aa001199a3fa308a4b424t253e2953";
+    let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}&units=imperial`;
+
+    axios.get(apiUrl).then(handleResponse);
+  }
+
   if (weatherData.ready) {
     return (
       <div className="Weather">
-        <form>
+        <form onSubmit={handleSubmit}>
           <div>
             <input
               type="search"
               autoFocus
               placeholder="Search for cities ..."
               className="search-input col-9"
+              onChange={handleCityChange}
             />
             <input className="col-3 submit-input" type="submit" Search />
           </div>
         </form>
-        <div className="row">
-          <div className="col-6 ">
-            <img
-              src="https://ssl.gstatic.com/onebox/weather/64/sunny.png"
-              alt="weather icon"
-            />
-          </div>
-
-          <div className="col-6 weather-heading">
-            <p className="text-center pt-5">
-              <FormattedDate date={weatherData.date} />
-            </p>
-            <h1>{weatherData.city}</h1>
-            <p className="text-capitalize">{weatherData.description}</p>
-            <h2>
-              {Math.round(weatherData.temperature)}
-              <span>°C | °F</span>
-            </h2>
-          </div>
-          <div className="weather-description">
-            <p>CONDITIONS</p>
-            <div className="row">
-              <div className="col-6">
-                <p>Feels like:</p>
-                <h3>{Math.round(weatherData.feelsLike)}°</h3>
-                <p>Humidity</p>
-                <h3>{weatherData.humidity}%</h3>
-              </div>
-              <div className="col-6">
-                <p>Wind Speed</p>
-                <h3>{Math.round(weatherData.windSpeed)} MPH</h3>
-
-                <p>Air Pressure</p>
-                <h3>{weatherData.pressure} PSI</h3>
-              </div>
-            </div>
-          </div>
-        </div>
+        <WeatherInfo data={weatherData} />
       </div>
     );
   } else {
-    const apiKey = "o7aa001199a3fa308a4b424t253e2953";
-    const apiUrl = `https://api.shecodes.io/weather/v1/current?query=${props.defaultCity}&key=${apiKey}&units=imperial`;
-    axios.get(apiUrl).then(handleResponse);
+    search();
     return <h4 className="text-center">Loading...</h4>;
   }
 }
---
+++ src/WeatherInfo.js
@@ -0,0 +1,47 @@
+import React from "react";
+import FormattedDate from "./FormattedDate";
+
+export default function WeatherInfo(props) {
+  return (
+    <div className="WeatherInfo">
+      <div className="row">
+        <div className="col-6 ">
+          <img
+            src="https://ssl.gstatic.com/onebox/weather/64/sunny.png"
+            alt="weather icon"
+          />
+        </div>
+
+        <div className="col-6 weather-heading">
+          <p className="text-center pt-5">
+            <FormattedDate date={props.data.date} />
+          </p>
+          <h1>{props.data.city}</h1>
+          <p className="text-capitalize">{props.data.description}</p>
+          <h2>
+            {Math.round(props.data.temperature)}
+            <span>°C | °F</span>
+          </h2>
+        </div>
+        <div className="weather-description">
+          <p>CONDITIONS</p>
+          <div className="row">
+            <div className="col-6">
+              <p>Feels like:</p>
+              <h3>{Math.round(props.data.feelsLike)}°</h3>
+              <p>Humidity</p>
+              <h3>{props.data.humidity}%</h3>
+            </div>
+            <div className="col-6">
+              <p>Wind Speed</p>
+              <h3>{Math.round(props.data.windSpeed)} MPH</h3>
+
+              <p>Air Pressure</p>
+              <h3>{props.data.pressure} PSI</h3>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
